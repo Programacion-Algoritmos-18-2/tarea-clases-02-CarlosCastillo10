86b730a433e609e30c0d7a7e8be8c354175ac75b
Carlos Castillo <carloscastillo090916@gmail.com>
2018-10-12 10:22:48 -0500

    Agregando ejemplo1 clase2

---
+++ Ejemplo2/src/ejemplo2/Precio.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo2;
+
+/**
+ *
+ * @author carlos
+ */
+public class Precio {
+    
+    double euros;
+    
+    public double obtener(){
+        
+        return euros;
+    }
+    
+    public void agregar(double x){
+        
+    }
+    
+    
+}
---
+++ Ejemplo2/src/ejemplo2/Principal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo2;
+
+/**
+ *
+ * @author carlos
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Precio p = new Precio();
+        
+        System.out.println(p.euros);
+    }
+    
+}
